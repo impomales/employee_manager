0ae0bb11286ee2094c270f127e7a3796796b1a09
Isaias Pomales <impomales@gmail.com>
2018-10-04 14:58:14 -0400

    add onChange handler to email

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import { CardSection, Card, Input, Button } from './common';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onEmailChange = this.onEmailChange.bind(this);
+  }
+
+  onEmailChange(text) {}
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="email@gmail.com" />
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange}
+          />
         </CardSection>
         <CardSection>
           <Input secureTextEntry label="password" placeholder="pass" />

6a4bcdb0701ac643263956f044f92d4a7d1dd632
Isaias Pomales <impomales@gmail.com>
2018-10-04 14:57:34 -0400

    define login reducer

---
+++ src/reducers/login.js
@@ -0,0 +1,30 @@
+const EMAIL_CHANGED = 'EMAIL_CHANGED';
+const PASSWORD_CHANGED = 'PASSWORD_CHANGED';
+
+export const emailChanged = text => ({
+  type: EMAIL_CHANGED,
+  payload: text
+});
+
+export const passwordChanged = text => ({
+  type: PASSWORD_CHANGED,
+  payload: text
+});
+
+const initialState = {
+  email: '',
+  password: ''
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case EMAIL_CHANGED:
+      return { ...state, email: action.payload };
+    case PASSWORD_CHANGED:
+      return { ...state, password: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

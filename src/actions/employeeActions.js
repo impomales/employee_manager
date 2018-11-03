35798647e3651ae8991ea3b6a22e0df0ca181ed3
Isaias Pomales <impomales@gmail.com>
2018-11-03 17:18:08 -0400

    wire up inputs with redux

--- src/actions/employeeActions.js
+++ src/actions/employeeActions.js
@@ -1,6 +1,8 @@
-import EMPLOYEE_UPDATE from './types';
+import { EMPLOYEE_UPDATE } from './types';
 
-export const employeeUpdate = ({ prop, value }) => ({
-  type: EMPLOYEE_UPDATE,
-  payload: { prop, value }
-});
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  };
+};
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
-class EmployeeCreate extends Component {
-  render() {
-    return (
-      <Card>
-        <CardSection>
-          <Input label="name" placeholder="Jane" />
-        </CardSection>
-        <CardSection>
-          <Input label="phone" placeholder="555-555-5555" />
-        </CardSection>
-        <CardSection>
+const EmployeeCreate = props => {
+  const { name, phone } = props;
+  return (
+    <Card>
+      <CardSection>
+        <Input
+          label="name"
+          placeholder="Jane"
+          value={name}
+          onChangeText={value =>
+            props.employeeUpdate({ prop: 'name', value })
+          }
+        />
+      </CardSection>
+      <CardSection>
+        <Input
+          label="phone"
+          placeholder="555-555-5555"
+          value={phone}
+          onChangeText={value =>
+            props.employeeUpdate({ prop: 'phone', value })
+          }
+        />
+      </CardSection>
+      <CardSection />
 
-        </CardSection>
+      <CardSection>
+        <Button>Create</Button>
+      </CardSection>
+    </Card>
+  );
+};
 
-        <CardSection>
-          <Button>
-            Create
-          </Button>
-        </CardSection>
-      </Card>
-    );
-  }
-}
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
 
-export default EmployeeCreate;
+const mapDispatchToProps = { employeeUpdate };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(EmployeeCreate);

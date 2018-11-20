a73864f9eaae75fdac4f13e2d4c0d8b2f2904af5
Isaias Pomales <impomales@gmail.com>
2018-11-20 18:52:16 -0500

    define employee fetch thunk

--- src/actions/employeeActions.js
+++ src/actions/employeeActions.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types';
 import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -19,6 +23,19 @@ export const employeeCreate = ({ name, phone, shift }) => {
       .push({ name, phone, shift })
       .then(() => {
         dispatch({ type: EMPLOYEE_CREATE });
-        Actions.employeeList({ type: 'reset' })
+        Actions.employeeList({ type: 'reset' });
       });
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return dispatch => {
+    firebase
+      .database()
+      .ref(`/user/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+      });
+  };
 };
